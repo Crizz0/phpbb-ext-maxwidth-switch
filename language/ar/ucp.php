dc2c0b2a4271570ed5f7ad5f92a81b36ea09bcb1
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-02-07 13:28:03 +0300

    Create ucp.php

---
+++ language/ar/ucp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package phpBB Extension - Crizzo Max-width switch - Arabic
+* @copyright (c) 2014 Crizzo - www.crizzo.de
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+ * Translated by Bassel Taha Alhitary (www.alhitary.net)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MAXWIDTH'			=> 'تكبير عرض المنتدى ',
+	'MAXWIDTH_EXPLAIN'	=> 'اختيارك "نعم" يعني استخدام الحد الأقصى لعرض المنتدى ( التصفح بكامل الشاشة ).',
+));
