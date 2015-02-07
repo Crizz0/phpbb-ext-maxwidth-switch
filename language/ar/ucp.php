0ea5e582d9f234e12eac14eded306da4bca138b9
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-02-07 16:53:40 +0300

    Update ucp.php

--- language/ar/ucp.php
+++ language/ar/ucp.php
@@ -4,7 +4,6 @@
 * @package phpBB Extension - Crizzo Max-width switch - Arabic
 * @copyright (c) 2014 Crizzo - www.crizzo.de
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
  * Translated by Bassel Taha Alhitary (www.alhitary.net)
 */
 
