cc43fe4435290f0aa9b181ab40b013afe23b9d57
max-ima <96241406+max-ima@users.noreply.github.com>
2022-05-02 13:52:09 +0200

    Slovenian
    Slovenian translation

---
+++ language/sl/ucp.php
@@ -0,0 +1,23 @@
+<?php
+/**
+*
+* @package phpBB Extension - Crizzo Max-width switch - English
+* @copyright (c) 2014 Crizzo - www.crizzo.de
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Slovenian Translation - Marko K.(max, max-ima,...)
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'MAXWIDTH'			=> 'Uporabite največjo širino',
+	'MAXWIDTH_EXPLAIN'	=> 'Z "da" lahko plošča uporablja celotno širino okna brskalnika.',
+));
