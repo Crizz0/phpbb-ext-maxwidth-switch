925b61f2d04b4c60fadacd3502d4fae01bfce5cb
max-ima <96241406+max-ima@users.noreply.github.com>
2022-05-05 09:34:39 +0200

    Update
    update

--- language/sl/ucp.php
+++ language/sl/ucp.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* @package phpBB Extension - Crizzo Max-width switch - English
+* @package phpBB Extension - Crizzo Max-width switch - Slovenia
 * @copyright (c) 2022 Crizzo - www.crizzo.de
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 * Slovenian Translation - Marko K.(max, max-ima,...)
